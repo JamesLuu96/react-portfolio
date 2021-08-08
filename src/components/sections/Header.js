9f39fe08661b6557a7cfd149be8718b220b247f4
JamesLuu96 <32526792+JamesLuu96@users.noreply.github.com>
2021-08-07 21:42:38 -0500

    styled navbar

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,9 +1,33 @@
 import React, {useState} from 'react'
-import {Flex, Button, Box, useColorMode, IconButton, useColorModeValue, Tooltip, Text} from '@chakra-ui/react'
+// import {Flex, Button, Box, useColorMode, IconButton, useColorModeValue, Tooltip, Text} from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Text,
+  Tooltip
+} from '@chakra-ui/react';
 import {SunIcon, MoonIcon} from "@chakra-ui/icons"
 import {usePage} from "../../globalState"
 import {Home} from "react-ionicons"
 import {useLogoColor} from "../../globalState"
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Dashboard', 'Projects', 'Team'];
+
 
 
 export default function Header() {
@@ -12,11 +36,32 @@ export default function Header() {
   const color = useColorModeValue('black', 'white')
   const { colorMode, toggleColorMode } = useColorMode()
   const {currentPage, setCurrentPage} = usePage()
-  const categories = ['Home', 'Projects', 'About Me', 'Contact']
+  const categories = ['Projects', 'About Me', 'Contact']
   const green = "linear(to-r, green.300, blue.500)"
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
+  const NavLink = ({ children }) => (
+    <Link
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      bgGradient={ currentPage === children ? green : undefined } 
+      color={currentPage === children ? "white": undefined}
+      {...(currentPage === children ? {_hover: {bgGradient: 'linear(to-r, blue.400, green.200)'}} : {})}
+      onClick={(e)=>{setCurrentPage(children);onClose(e)}}
+      fontWeight="bold"
+      >
+      {children}
+    </Link>
+  );
+
   return (
     <>
-    <Tooltip label={colorMode === "light" ? "Toggle Darkmode?" : "Toggle Lightmode?"}  >
+    {/* <Tooltip label={colorMode === "light" ? "Toggle Darkmode?" : "Toggle Lightmode?"}  >
       <IconButton borderRadius="var(--chakra-radii-md)" position="fixed" right={0} m={3} zIndex={11} aria-label="Toggle Darkmode" icon={colorMode === "light" ? <SunIcon/> : <MoonIcon/>} onClick={toggleColorMode}/>
     </Tooltip>
     <Box bgGradient={green} w="100%" h="4px" mb="20px"/>
@@ -47,7 +92,49 @@ export default function Header() {
         {category !== "Home" ? category : <Home color={color}/>}
       </Button>
       ))}
-    </Flex>
+    </Flex> */}
+    {/* <> */}
+      <Box bgGradient={green} w="100%" h="4px"/>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} position="sticky" top={0} mb={10} zIndex="10" >
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Flex fontSize="24px" fontWeight="bold" className="flipParent" onClick={(e)=>{setCurrentPage("Home");onClose(e)}}>
+                <Text>J</Text>
+                <Text className="flip" color={logoColor}>K</Text>
+                <Text>L</Text>
+            </Flex>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {categories.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <IconButton icon={colorMode === "light" ? <SunIcon/> : <MoonIcon/>} onClick={toggleColorMode}/>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {categories.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
     </>
+    // </>
   )
 }
